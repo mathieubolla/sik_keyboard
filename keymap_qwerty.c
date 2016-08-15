0d6644e750e904bc45bf028890ebe6ed8b90bd9b
Mathieu Bolla <mathieu.bolla@gmail.com>
2016-08-15 21:44:40 +0200

    Add an en-us layout 'lookalike'

--- keymap_qwerty.c
+++ keymap_qwerty.c
@@ -14,19 +14,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_P7,    KC_P8,    KC_P9,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_6,    KC_NO,    KC_NO,
       KC_NUBS,  KC_LBRC,  KC_QUOT,    KC_DOT,    KC_P4,    KC_P5,    KC_P6,     KC_5,  KC_RBRC,  KC_MINS,  KC_SLSH,  KC_TRNS,  KC_TRNS,
       KC_1,     KC_TRNS,     KC_3,    KC_GRV,    KC_P1,    KC_P2,    KC_P3,  KC_NUHS,     KC_9,  KC_TRNS,     KC_8,  KC_TRNS,  KC_TRNS,
-      KC_7,     KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_P0,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_FN3,  KC_TRNS,  KC_TRNS),
+      KC_7,     KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_P0,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_FN4,   KC_FN3,  KC_TRNS,  KC_TRNS),
   KEYMAP(
-      KC_FN0,   KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_F7,      KC_F8,    KC_F9,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_NO,    KC_NO,
-      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_F4,      KC_F5,    KC_F6,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_F1,      KC_F2,    KC_F3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_F10,    KC_F11,   KC_F12,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
+      KC_COMM,     KC_G,     KC_F,      KC_V,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_Q,     KC_B,     KC_H,  KC_SCLN,    KC_NO,    KC_NO,
+      KC_MINS,     KC_T,     KC_A,      KC_E,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_S,     KC_I,     KC_N,  KC_QUOT,  KC_LALT,   KC_TAB,
+         KC_W,     KC_U,     KC_D,      KC_L,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_O,     KC_C,     KC_M,     KC_P,   KC_FN5,   KC_SPC,
+       KC_DOT,   KC_FN2,     KC_Y,      KC_J,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_X,     KC_Z,     KC_K,     KC_3,  KC_LGUI,  KC_LSFT),
+  KEYMAP(
+      KC_SLSH,  KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_P7,    KC_P8,    KC_P9,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_8,    KC_NO,    KC_NO,
+      KC_BSLS,     KC_6,     KC_5,   KC_TRNS,    KC_P4,    KC_P5,    KC_P6,     KC_9,     KC_4,  KC_TRNS,   KC_EQL,  KC_TRNS,  KC_TRNS,
+         KC_7,  KC_TRNS,  KC_LBRC,      KC_2,    KC_P1,    KC_P2,    KC_P3,   KC_GRV,  KC_RBRC,  KC_TRNS,     KC_1,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_P0,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_FN3,  KC_TRNS,  KC_TRNS),
+  KEYMAP(
+      KC_FN0,   KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_F7,    KC_F8,    KC_F9,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_NO,    KC_NO,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_F4,    KC_F5,    KC_F6,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,    KC_F1,    KC_F2,    KC_F3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+      KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_F10,   KC_F11,   KC_F12,  KC_TRNS,  KC_TRNS,   KC_FN4,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_FUNCTION(BOOTLOADER),
   [1] = ACTION_LAYER_MOMENTARY(1),
-  [2] = ACTION_LAYER_TAP_KEY(2, KC_R),
-  [3] = ACTION_MODS_KEY(MOD_LSFT|MOD_LCTL, KC_POWER)
+  [2] = ACTION_LAYER_TAP_KEY(4, KC_R),
+  [3] = ACTION_MODS_KEY(MOD_LSFT|MOD_LCTL, KC_POWER),
+  [4] = ACTION_LAYER_TOGGLE(2),
+  [5] = ACTION_LAYER_MOMENTARY(3)
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
